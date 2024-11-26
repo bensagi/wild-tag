ab8620a5509c32ec3ec4db847abb65af0a8565ac
bensagi <bensagi@cisco.com>
2024-11-26 13:48:28 +0200

    asd

--- src/main/java/applications/Tagger.java
+++ src/main/java/applications/Tagger.java
@@ -1,21 +1,16 @@
 package applications;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.util.List;
-import management.entities.images.CoordinateDB;
 import management.entities.images.ImageDB;
-import management.entities.images.ImageStatus;
-import management.repositories.ImagesRepository;
 import management.services.ImageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "management")
@@ -26,10 +21,13 @@ public class Tagger implements CommandLineRunner {
   private Logger logger = LoggerFactory.getLogger(Tagger.class);
 
   @Autowired ImageService imageService;
+
+  @Value("${tagger.batchSize:100}")
   private int limit;
 
   public static void main(String[] args) {
     SpringApplication.run(Tagger.class, args);
+    System.exit(0);
   }
 
   @Override
@@ -41,6 +39,7 @@ public class Tagger implements CommandLineRunner {
       List<ImageDB> images = imageService.getValidatedImages(limit);
       logger.debug("found {} images to build tag", images.size());
       if (images.isEmpty()) {
+        logger.info("tagging proces finished. handled {} images", handled);
         return;
       }
 
@@ -54,7 +53,7 @@ public class Tagger implements CommandLineRunner {
           logger.error("failed to tag image {}", image.getId(), e);
         }
       }
-      logger.info("handled {} images", images);
+      logger.info("handled {} images", handled);
     }
   }
 }
